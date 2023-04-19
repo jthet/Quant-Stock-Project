e5070dd53623b2eefb6ee1976e4113635981e514
Jackson Thetford <j.thetford@utexas.edu>
2023-04-19 21:01:36 +0000

    cleaning

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM python:3.8.10
 
 RUN pip install Flask==2.2.0 redis requests matplotlib==3.7.1
 
-ADD ./gene_api.py /gene_api.py
+ADD ./quant_stock_api.py /quant_stock_api.py
 
-CMD ["python3", "/gene_api.py"]
+CMD ["python3", "/quant_stock_api.py"]
---
+++ quant_stock_api.py
@@ -0,0 +1,71 @@
+from flask import Flask, request, send_file
+import redis
+import requests
+import json
+import os
+import matplotlib.pyplot as plt
+import io
+
+app = Flask(__name__)
+
+def get_redis_client():
+    redis_ip = os.environ.get('REDIS-IP')
+    if not redis_ip:
+        raise Exception()
+    return redis.Redis(host=redis_ip, port=6379, db=0, decode_responses=True)
+
+def get_redis_image_db():
+    redis_ip = os.environ.get('REDIS-IP')
+    if not redis_ip:
+        raise Exception()
+    return redis.Redis(host=redis_ip, port=6379, db=1)
+
+rd = get_redis_client()
+
+rd_image = get_redis_image_db()
+
+@app.route('/data', methods = ['GET', 'POST', 'DELETE'])
+def handle_data() -> list:
+    '''
+    Manipulates data wiht 3 different methods with GET, POST, and DELETE method
+
+    Args: None
+
+    Returns: String corresponding to which method was used
+        "DELETE" method: deletes all data in redis db
+        "POST" method: posts data into redis db
+        "GET" method: returns data from redis db
+    '''
+
+    method = request.method
+    global rd
+
+    if method == 'POST':
+        response = requests.get(url = 'https://ftp.ebi.ac.uk/pub/databases/genenames/hgnc/json/hgnc_complete_set.json')
+        data = response.json()
+
+        for item in data['response']['docs']:
+            rd.set(item.get('hgnc_id'), json.dumps(item))
+        return f'Data Loaded there are {len(rd.keys())} keys in the db\n'
+    
+    elif method == 'GET':
+        output_list = []
+        keys = rd.keys()
+        for key in keys:
+            output_list.append(json.loads(rd.get(key)))
+        return output_list
+        
+
+    elif method == 'DELETE':
+        rd.flushdb()
+        return f'data deleted, there are {len(rd.keys())} keys in the db\n'
+
+    else:
+        return 'the method you tried is not supported\n'
+
+    return f'method completed \n'
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host='0.0.0.0')
